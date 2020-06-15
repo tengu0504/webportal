f26ce47bdbe6148f99da55d8423d7f3875ebeac0
negim <negim@DESKTOP-HMAPO9D>
2020-06-15 14:10:54 +0900

    first commit

---
+++ src/main/java/jp/ac/hcs/s3a227/WebportalApplication.java
@@ -0,0 +1,13 @@
+package jp.ac.hcs.s3a227;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebportalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/ac/hcs/s3a227/WebportalApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ac.hcs.s3a227;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
